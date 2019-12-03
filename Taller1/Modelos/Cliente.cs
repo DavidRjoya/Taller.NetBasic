34a49a8eed7fd2a183be1ce33f999ecf1f6583a8
David Joya <davidaj918@gmail.com>
2019-12-02 22:11:28 -0500

    Creacion de Proyecto

---
+++ Taller1/Modelos/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller1.Modelos
+{
+    public class Cliente
+    {
+        public Cliente(String nombre, String apellido, DateTime fechanacimiento) {
+            
+            Nombre = nombre;
+            Apellido = apellido;
+            FechaNacimiento = fechanacimiento;
+            Edad = DateTime.Today.AddTicks(-fechanacimiento.Ticks).Year - 1;
+        }
+
+        public Cliente(String direccion)
+        {
+            Direccion = direccion;
+        }
+
+        public Cliente()
+        {
+            
+        }
+
+        public String Nombre, Apellido, Direccion;
+        public int Telefono, Edad;
+        public DateTime FechaNacimiento;
+
+        
+
+    }
+
+}
---
+++ Taller1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Taller1
+{
+    using Modelos;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+          Cliente nuevoCliente;
+
+            nuevoCliente = new Cliente("david","Alvarado",new DateTime(1999,7,2));
+
+
+            //nuevoCliente.Nombre = "David";
+            //nuevoCliente.Apellido = "Joya";
+            //nuevoCliente.Edad = 19;
+            //Clae
+       
+            Console.WriteLine($"Cliente 1 {nuevoCliente.Nombre} {nuevoCliente.Apellido} {nuevoCliente.Edad}");
+
+            Cliente nuevoCliente2 = new Cliente("kr 95 a");
+            nuevoCliente2.Nombre = "DAVID";
+            Console.WriteLine($"Cliente 1 {nuevoCliente2.Nombre} {nuevoCliente2.Apellido} {nuevoCliente2.Edad} {nuevoCliente2.Direccion}");
+
+
+
+
+            //getter obetener valor por defecto
+        }
+    }
+}
